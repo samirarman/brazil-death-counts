ce5f7267f39305a0ffe937939bb10bd6812aba36
samir <samir.arman@gmail.com>
2020-06-15 12:27:31 -0300

    Saves targets to disk as merged data

--- make_sanity_report.R
+++ make_sanity_report.R
@@ -7,8 +7,11 @@ library(readxl)
 
 
 read_files <- function(dir) {
-    list.files(path = dir, pattern = "*.csv", full.names = TRUE) %>%
-        map_dfr(read_csv,
+    list.files(path = dir,
+               pattern = "*.csv",
+               full.names = TRUE) %>%
+        map_dfr(
+            read_csv,
             col_types = c(
                 X1 = col_double(),
                 Estado = col_character(),
@@ -19,30 +22,27 @@ read_files <- function(dir) {
             )
         ) %>%
         select(-c(1)) %>%
-        mutate(Mês = factor(Mês, levels = MONTHS))
+        mutate(Mês = factor(Mês, levels = MONTHS)) %>%
+        write_csv(
+            paste(
+                "./merged_data/",
+                str_split(dir, "/") %>% last() %>% last(),
+                ".csv",
+                sep = "")
+        )
 }
 
 plan <- drake_plan(
-    cities_files = target(
-        "./scraped_data/cities/",
-        format = "file"
-    ),
-    cities_summary_files = target(
-        "./scraped_data/cities_summary/",
-        format = "file"
-    ),
-    states_files = target(
-        "./scraped_data/states",
-        format = "file"
-    ),
-    states_summary_files = target(
-        "./scraped_data/states_summary",
-        format = "file"
-    ),
-    ibge_file = target(
-        "RELATORIO_DTB_BRASIL_MUNICIPIO.xls",
-        format = "file"
-    ),
+    cities_files = target("./scraped_data/cities",
+                          format = "file"),
+    cities_summary_files = target("./scraped_data/cities_summary",
+                                  format = "file"),
+    states_files = target("./scraped_data/states",
+                          format = "file"),
+    states_summary_files = target("./scraped_data/states_summary",
+                                  format = "file"),
+    ibge_file = target("RELATORIO_DTB_BRASIL_MUNICIPIO.xls",
+                       format = "file"),
     cities_df = read_files(cities_files),
     cities_summary_df = read_files(cities_summary_files),
     states_df = read_files(states_files),
