0071d5c03e2e37712ca24fca490e386219685a32
Samir Arman <samir.arman@gmail.com>
2020-07-15 20:03:41 -0300

    Minor correction

--- run.sh
+++ run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-rm ./out/*
 
-nohup java -Dwebdriver.gecko.driver=/usr/bin/geckodriver -jar ~/.local/share/binman_seleniumserver/generic/3.141.59/selenium-server-standalone-3.141.59.jar -port 4545 > ./out/server.out &
+nohup java -Dwebdriver.gecko.driver=/usr/bin/geckodriver -jar /home/.local/share/binman_seleniumserver/generic/3.141.59/selenium-server-standalone-3.141.59.jar -port 4545 > ./merged_data/out/server.out &
 
 sleep 30
 
-nohup Rscript make_data.R 2015 2020 "Todos" "" "Todos" "./merged_data/by_state_yearly.csv" > ./out/by_state_yearly.out &
+nohup Rscript make_data.R 2015 2020 "Todos" "" "Todos" "./merged_data/by_state_yearly.csv" > ./merged_data/out/by_state_yearly.out &
 job1=$!
 echo "Starting job $job1"
 
-nohup Rscript make_data.R 2015 2020 "1" "12" "Todos" "./merged_data/by_state_monthly.csv" > ./out/by_state_monthly.out &
+nohup Rscript make_data.R 2015 2020 "1" "12" "Todos" "./merged_data/by_state_monthly.csv" > ./merged_data/out/by_state_monthly.out &
 job2=$!
 echo "Starting job $job2"
 
@@ -21,7 +20,7 @@ wait $job2
 echo "Finished job $job2"
 
 
-nohup Rscript make_data.R 2015 2020 "Todos" "" "" "./merged_data/by_city_yearly.csv" > ./out/by_city_yearly.out &
+nohup Rscript make_data.R 2015 2020 "Todos" "" "" "./merged_data/by_city_yearly.csv" > ./merged_data/out/by_city_yearly.out &
 job=$!
 echo "Starting job $job"
 
@@ -29,11 +28,11 @@ wait $job
 echo "Finished job $job"
 
 
-nohup Rscript make_data.R 2015 2020 "1" "3" "" "./merged_data/by_city_monthly1.csv" > ./out/by_city_monthly.out &
+nohup Rscript make_data.R 2015 2020 "1" "3" "" "./merged_data/by_city_monthly1.csv" > ./merged_data/out/by_city_monthly.out &
 job1=$!
 echo "Starting job $job1"
 
-nohup Rscript make_data.R 2015 2020 "4" "6" "" "./merged_data/by_city_monthly2.csv" > ./out/by_city_monthly.out &
+nohup Rscript make_data.R 2015 2020 "4" "6" "" "./merged_data/by_city_monthly2.csv" > ./merged_data/out/by_city_monthly.out &
 job2=$!
 echo "Starting job $job2"
 
@@ -44,11 +43,11 @@ echo "Finished job $job2"
 
 
 
-nohup Rscript make_data.R 2015 2020 "7" "9" "" "./merged_data/by_city_monthly3.csv" > ./out/by_city_monthly.out &
+nohup Rscript make_data.R 2015 2020 "7" "9" "" "./merged_data/by_city_monthly3.csv" > ./merged_data/out/by_city_monthly.out &
 job1=$!
 echo "Starting job $job1"
 
-nohup Rscript make_data.R 2015 2020 "10" "12" "" "./merged_data/by_city_monthly4.csv" > ./out/by_city_monthly.out &
+nohup Rscript make_data.R 2015 2020 "10" "12" "" "./merged_data/by_city_monthly4.csv" > ./merged_data/out/by_city_monthly.out &
 job2=$!
 echo "Starting job $job2"
 
@@ -59,7 +58,7 @@ wait $job2
 echo "Finished job $job2"
 
 
-nohup Rscript merge_data.R > ./out/merge_data.out
+nohup Rscript merge_data.R > ./merged_data/out/merge_data.out
 
 for i in {1..4}
 do
